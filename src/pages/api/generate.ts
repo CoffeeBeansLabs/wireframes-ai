de88eb4b7c28f65e37b20a761b95c206190cd49d
Darshak Parikh <darshak@coffeebeans.io>
2024-12-13 23:42:49 +0530

    Save conversations in memory

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -2,6 +2,7 @@ import type { APIRoute } from "astro";
 import Anthropic from "@anthropic-ai/sdk";
 
 import dotenv from "dotenv";
+import { v6 as uuidv6 } from "uuid";
 import buildPrompt from "../../utils/prompt";
 
 dotenv.config();
@@ -10,14 +11,39 @@ const client = new Anthropic({
   apiKey: process.env.ANTHROPIC_API_KEY,
 });
 
+const conversations = new Map();
+const prompts = new Map();
+
 export const POST: APIRoute = async ({ request }) => {
   try {
-    const { userInput } = await request.json();
+    const { initialInput, subsequentMessage, conversationId } =
+      await request.json();
 
-    const message = await client.messages.create(buildPrompt(userInput));
+    const prompt = initialInput
+      ? buildPrompt(initialInput)
+      : prompts.get(conversationId);
+
+    if (conversationId && subsequentMessage) {
+      prompt.messages.push({
+        role: "user" as const,
+        content: subsequentMessage,
+      });
+    }
+    const assistantMessage = await client.messages.create(prompt);
+    const currentConversationId = conversationId ?? uuidv6();
+
+    conversations.set(currentConversationId, [
+      ...prompt.messages,
+      assistantMessage,
+    ]);
+
+    prompts.set(currentConversationId, prompt);
 
     return new Response(
-      JSON.stringify({ response: (message.content[0] as any).input }),
+      JSON.stringify({
+        response: (assistantMessage.content[0] as any).input,
+        conversationId,
+      }),
       {
         status: 200,
         headers: {
