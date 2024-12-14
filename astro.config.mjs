fdca7274915b1f0c53b548024d5194db35293704
Darshak Parikh <darshak@coffeebeans.io>
2024-12-14 17:41:00 +0530

    Try again

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@ import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
 
-import netlify from "@astrojs/netlify";
-
 import vercel from "@astrojs/vercel";
 
 export default defineConfig({
