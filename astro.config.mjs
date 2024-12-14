159d1909c7ecef23cfa5e919764705c668e50929
Darshak Parikh <darshak@coffeebeans.io>
2024-12-14 16:29:47 +0530

    Upgrade Astro and Preact, and add Netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@ import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
 
+import netlify from "@astrojs/netlify";
+
 export default defineConfig({
   integrations: [preact(), tailwind()],
   output: "server",
+  adapter: netlify(),
 });
